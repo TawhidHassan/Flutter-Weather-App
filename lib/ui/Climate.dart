e8a9be3b9f6a6b1eeda5ad9aa96e6e7cc2f001b1
sifat007 <sifat27.ht@gmail.com>
2020-12-31 10:57:45 +0600

    dynamic city name done

--- lib/ui/Climate.dart
+++ lib/ui/Climate.dart
@@ -8,23 +8,38 @@ class Climate extends StatefulWidget {
 }
 
 class _ClimateState extends State<Climate> {
-
   void showStuff() async {
     Map data = await getWeather(util.apiId, util.defaultCity);
     print(data.toString());
+  }
+
+  String _cityEntered;
+  Future _goToNextScreen(BuildContext context) async {
+    Map results = await Navigator.of(context)
+        .push(new MaterialPageRoute<Map>(builder: (BuildContext context) {
+      //change to Map instead of dynamic for this to work
+      return new ChangeCity();
+    }));
+
+    if (results != null && results.containsKey('enter')) {
+      _cityEntered = results['enter'];
+
+//      debugPrint("From First screen" + results['enter'].toString());
+
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('ClimateApp'),
         backgroundColor: Colors.red,
-        title: Text("Home"),
         actions: <Widget>[
           IconButton(
               icon: Icon(Icons.menu),
               onPressed: () {
-                showStuff();
+                _goToNextScreen(context);
               })
         ],
       ),
@@ -41,7 +56,8 @@ class _ClimateState extends State<Climate> {
           Container(
             alignment: Alignment.topRight,
             margin: EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
-            child: Text("Dahaka",
+            child: Text(
+              '${_cityEntered == null ? util.defaultCity : _cityEntered}',
               style: cityStyle(),
             ),
           ),
@@ -52,66 +68,111 @@ class _ClimateState extends State<Climate> {
           ),
           Container(
             margin: EdgeInsets.fromLTRB(30.0, 90.0, 0.0, 0.0),
-            child: updateTempWidget("Dhaka"
-          ),
+            child: updateTempWidget(
+                '${_cityEntered == null ? util.defaultCity : _cityEntered}'),
           ),
         ],
       ),
     );
   }
 
+  Future<Map> getWeather(String appId, String city) async {
+    String apiUrl =
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid='
+        '${util.apiId}&units=imperial';
+    //http://api.openweathermap.org/data/2.5/weather?q=vehari&appid=ee595cbdb0db0ef5d11b9caf5a8eb1ea&units=metric
+    http.Response response = await http.get(apiUrl);
 
-}
+    return json.decode(response.body);
+  }
 
-Widget updateTempWidget(String city) {
-  return FutureBuilder(
-      future: getWeather(util.apiId, city == null ? util.defaultCity : city),
-      builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
-        //where we get all of the json data, we setup widgets etc.
-        if (snapshot.hasData) {
-          Map content = snapshot.data;
-          return Container(
-            margin: const EdgeInsets.fromLTRB(30.0, 250.0, 0.0, 0.0),
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new ListTile(
-                  title: new Text(
-                    content['main']['temp'].toString() + " F "+content['weather'][0]['main'],
-                    style: new TextStyle(
-                        fontStyle: FontStyle.normal,
-                        fontSize: 49.9,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  subtitle: new ListTile(
+  Widget updateTempWidget(String city) {
+    return FutureBuilder(
+        future: getWeather(util.apiId, city == null ? util.defaultCity : city),
+        builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
+          //where we get all of the json data, we setup widgets etc.
+          if (snapshot.hasData) {
+            Map content = snapshot.data;
+            return Container(
+              margin: const EdgeInsets.fromLTRB(30.0, 250.0, 0.0, 0.0),
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new ListTile(
                     title: new Text(
-                      "Humidity: ${content['main']['humidity'].toString()}\n"
-                          "Min: ${content['main']['temp_min'].toString()} F\n"
-                          "Max: ${content['main']['temp_max'].toString()} F ",
-                      style: extraData(),
+                      content['main']['temp'].toString() + " F",
+                      style: new TextStyle(
+                          fontStyle: FontStyle.normal,
+                          fontSize: 49.9,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500),
                     ),
-                  ),
-                )
-              ],
-            ),
-          );
-        } else {
-          return Container();
-        }
-      });
+                    subtitle: new ListTile(
+                      title: new Text(
+                        "Humidity: ${content['main']['humidity'].toString()}\n"
+                            "Min: ${content['main']['temp_min'].toString()} F\n"
+                            "Max: ${content['main']['temp_max'].toString()} F ",
+                        style: extraData(),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          } else {
+            return Container();
+          }
+        });
+  }
 }
 
-
-Future<Map>getWeather(String appId,String city)async {
-
-  String apiUrl =
-      'https://api.openweathermap.org/data/2.5/weather?q=$city&appid='
-      '${util.apiId}&units=imperial';
-  //http://api.openweathermap.org/data/2.5/weather?q=vehari&appid=5fe06374c9ac1b1981c3c1b80da00d77&units=metric
-  http.Response response = await http.get(apiUrl);
-
-  return json.decode(response.body);
+class ChangeCity extends StatelessWidget {
+  var _cityFieldController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Change City'),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Image.asset(
+              'images/white_snow.png',
+              width: 590.0,
+              height: 1200.0,
+              fit: BoxFit.fill,
+            ),
+          ),
+          ListView(
+            children: <Widget>[
+              ListTile(
+                title: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter City',
+                  ),
+                  controller: _cityFieldController,
+                  keyboardType: TextInputType.text,
+                ),
+              ),
+              ListTile(
+                title: FlatButton(
+                    onPressed: () {
+                      Navigator.pop(
+                          context, {'enter': _cityFieldController.text});
+                    },
+                    textColor: Colors.white70,
+                    color: Colors.redAccent,
+                    child: Text('Get Weather')),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
 
 TextStyle cityStyle() {
